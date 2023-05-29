62db89f6559407354f7e20eeeb985acb71fca97a
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-29 23:06:46 +0100

    Add category page logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,33 @@
 class CategoriesController < ApplicationController
 
   def index
-
+    @categories = Category.includes(:operations).where(author_id: current_user.id)
   end
 
   def show
 
+  end
+
+  def new
+
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author_id = current_user.id
+    if @category.save
+      flash[:notice] = "Category created successfully"
+      redirect_to categories_path
+    else
+      flash[:alert] = "Category not created"
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :author_id, :icon)
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
-class Catgeory < ApplicationRecord
+class Category < ApplicationRecord
 
   validates :name, presence: true
   validates :icon, presence: true
 
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
   has_many :entity_groups, dependent: :destroy
   has_many :entities, through: :entity_groups
 
