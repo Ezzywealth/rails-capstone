ee7c388072743a6af0a392d17ac55a131e64efd9
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-01 21:35:37 +0100

    Remove authentication for splash screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!, except: :welcome
+  before_action :authenticate_user!, unless: :skip_authentication?
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -20,5 +18,13 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource)
     root_path
   end
+
+  def skip_authentication?
+    # Return true if the current page should be excluded from authentication
+    # You can use any condition you like to determine if authentication should be skipped
+    # For example, to skip authentication for the welcome action of the welcome controller:
+    params[:controller] == 'welcome' && params[:action] == 'index'
+  end
+
 
 end
